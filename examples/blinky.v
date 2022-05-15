9f646e5677c8359da98e661ce161c29440e45f7a
YRabbit <rabbit@yrabbit.cyou>
2022-05-15 10:16:34 +1000

    wip
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- examples/blinky.v
+++ examples/blinky.v
@@ -5,7 +5,6 @@ module top (
 
 reg [25:0] ctr_q;
 wire [25:0] ctr_d;
-wire [`LEDS_NR-1:0]w_out;
 
 // Sequential code (flip-flop)
 always @(posedge clk)
@@ -13,7 +12,6 @@ always @(posedge clk)
 
 // Combinational code (boolean logic)
 assign ctr_d = ctr_q + 1'b1;
-assign w_out = ~ctr_q[25:25-(`LEDS_NR - 1)];
-assign led = w_out;
+assign led = ctr_q[25:25-(`LEDS_NR - 1)];
 
 endmodule
