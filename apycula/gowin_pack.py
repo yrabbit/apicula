39578f8d459f96771133257f78d6ef18b2056df3
YRabbit <rabbit@yrabbit.cyou>
2023-01-05 13:57:40 +1000

    wip
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- apycula/gowin_pack.py
+++ apycula/gowin_pack.py
@@ -551,7 +551,7 @@ def secure_long_wires(db, tilemap, row, col, src, dest):
         # the column runs across the entire height of the chip from the first to the last row
         check_row = db.rows - 1
         fuse_row = 0
-        if row == check_row and dest in {'LT02', 'LT13', 'E210'}:
+        if row == check_row and dest in {'LT02', 'LT13'}:
             tiledata = db.grid[fuse_row][col - 1]
             if dest in tiledata.alonenode_6.keys():
                 tile = tilemap[(fuse_row, col - 1)]
