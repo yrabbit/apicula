63c13138779206d46dcc0e2f1dc404c578907acf
YRabbit <rabbit@yrabbit.cyou>
2023-01-24 18:46:14 +1000

    wip
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- apycula/bench-empty.py
+++ apycula/bench-empty.py
@@ -281,8 +281,8 @@ if __name__ == "__main__":
                     print(ttyp, src)
     """
 
-    row3 = 0
-    col3 = 27
+    row3 = 9
+    col3 = 43
     #row3 = 0
     #col3 = 17
     import ipdb; ipdb.set_trace()
